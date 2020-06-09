5830bb570c8dc732b55d5496c6f4c56540567713
Ishani <ishanipathak@gmail.com>
2020-06-09 17:24:36 +0530

    Updating the code to be PWA compliant, added service worker, https, as well as updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import {store , persistor } from './redux/Store'
+import * as serviceWorker from './serviceWorker'
 
 
 ReactDOM.render(
@@ -18,4 +19,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const path= require('path') // allows to dynamically build
+const enforce = require('express-sslify')
 
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config()
@@ -21,11 +22,12 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }))
-
+app.use(enforce.HTTPS({ trustProtoHeader : true }))
 app.use(cors())
 
 if(process.env.NODE_ENV === 'production')
 {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')))
     app.get('*' , function( req, res){
         res.sendFile(path.join(__dirname , 'client/build' , 'index.html'))
@@ -36,6 +38,10 @@ if(process.env.NODE_ENV === 'production')
 app.listen(port, error =>{
     if(error) throw error
     console.log('Server running on port:' + port)
+})
+
+app.get('/service-worker.js',(req, res) =>{
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
 })
 
 
